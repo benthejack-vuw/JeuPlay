880dbddda71be75a5e8bec39815cf8b436fdd1a6
Ben Jack <ben.jack@vuw.ac.nz>
2018-03-26 21:12:37 +1300

    config set up for pi testing

---
+++ JP_Pi_Server/raspi_instruments/ChannelSubscriber.rb
@@ -0,0 +1,33 @@
+class ChannelSubscriber
+
+  DELEGATE_ERROR = "ChannelSubscriber's delegate must implement a timed_event method"
+  LOOP_DELAY = 1.0/100.0 #50th of a second
+
+  def initialize delegate, port
+    @delegate = delegate
+    @port = port
+    raise @@delegate_error unless @delegate.respond_to? "timed_event"
+  end
+
+  def connect
+		begin
+      @socket = UDPSocket.new
+      @socket.bind("0.0.0.0", @port)
+		rescue
+		end
+		puts "listening on port #{@port}"
+  end
+
+  def listen
+
+      begin
+        text = @socket.recvfrom(16)
+        @delegate.timed_event if text[0].include?('bang')
+    		sleep LOOP_DELAY
+      rescue
+      end
+
+      false #return false runs forever if used in block_loop - OWL.rb
+  end
+
+end
