509a6bed1cf5b5733925e981ce34e96087e6e896
Ben Jack <ben.jack@vuw.ac.nz>
2018-03-28 10:13:39 +1300

    added test instrument

---
+++ JP_Pi_Server/raspi_instruments/testInstrument.rb
@@ -0,0 +1,37 @@
+require_relative "raspiInstrument"
+
+class TestInstrument < RaspiInstrument
+
+  def initialize
+    super 1
+    @count = 0
+    @beat = 0;
+    connect_to_timing_server
+    run
+  end
+
+  def run
+    while true
+      @channel_sub.listen
+      sleep 0.01
+    end
+  end
+
+  def timed_event
+    play(["test", "data"]) if @data.rhythm[@beat] == '1'
+		@beat = (@beat+1) % @data.rhythm.length
+  end
+
+  def play args
+    puts "#{@count}"
+    Thread.new{
+    `aplay ./sounds/#{@data.sound}`
+    }
+    @count += 1
+  end
+
+end
+
+if __FILE__ == $0
+	instrument = TestInstrument.new ARGV[0].to_i
+end
