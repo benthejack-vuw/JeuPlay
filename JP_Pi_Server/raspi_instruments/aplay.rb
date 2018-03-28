6f620c6c397207267a3a9c50aa8b6201fbd0c1b5
Ben Jack <ben.jack@vuw.ac.nz>
2018-03-29 12:32:01 +1300

    async

--- JP_Pi_Server/raspi_instruments/aplay.rb
+++ JP_Pi_Server/raspi_instruments/aplay.rb
@@ -1,31 +1,31 @@
 require_relative 'pipe'
 
 class Aplay
-  
+
   def initialize
     @running = true
     @ip = create_pipe
   end
-  
+
   def run
     @running = true
     while @running
      x = @ip.read.chomp
-     %x{afplay #{x}}
+     %x{aplay #{x}}
      @running = false if x == "stop"
     end
     shutdown
   end
-  
+
   def shutdown
     @ip.close
   end
-  
+
   def create_pipe
-    %x{ rm "#{self.class.name}_IN" } if File.exists?( "#{self.class.name}_IN.99")
+    %x{ rm "#{self.class.name}_IN" } if File.exists?( "#{self.class.name}_IN")
     Pipe.new("#{self.class.name}_IN").tap{|p| p.open_for_input }
   end
-  
+
 end
 
 # if __FILE__ == $0
